43de704cc693eebe9221eaf516a39a0b4dce888c
Boots2291 <buteramv@gmail.com>
2018-09-26 12:30:29 -0400

    Add delete and edit functionality for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,6 +16,24 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    @project.update(project_params)
+
+    redirect_to '/projects'
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.delete
+
+    redirect_to '/projects'
+  end
+
   private
 
   def project_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  get 'projects', to: 'projects#index'
-  get '/projects/new', to: 'projects#new'
-  post 'projects', to: 'projects#create'
+  resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
