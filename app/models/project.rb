e3a92055b84e94e0013a351ac3d68523a0e0de15
Luis  Senior <heybluguy@yahoo.com>
2018-11-07 00:08:28 -0600

    Added seed data and image. Corrected button's text sizes in mobile horizontal orientation.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,3 @@
 class Project < ApplicationRecord
-   validates :name, :desc, :img, :hosted_url, :github_url, presence: true
+   validates :name, :desc, :hosted_url, :github_url, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-Project.create(name: "Milo's Pineapple Shop",
-  desc: "An e-commerce platform built with Ruby on Rails and utilizes Stripe API for payment processing, to buy and sell clothing to or from other users. Upon signing up, a user could post their own new items to sell or view listings from other users. The listing shows the brand, size, title, price, description and image of the item.",
-  hosted_url: "https://lit-chamber-46433.herokuapp.com/",
-  github_url: "https://github.com/Heybluguy/milos_pineapple_shop"
-)
+5.times do |index|
+  Project.create(name: "My Project",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    img: "https://loremipsum.io/assets/images/lorem-ipsum-generator-word-processor.png",
+    hosted_url: "https://app.herokuapp.com/",
+    github_url: "https://github.com/Me/my-app"
+  )
+end
