113cf768e55678f6a3fa414667d3db9572985ad7
Luis  Senior <heybluguy@yahoo.com>
2018-12-06 02:54:56 -0600

    Adds resume to root path

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   get 'resumes/destroy'
   root to: 'home#index'
   resources :projects
-  get '/login'     => 'sessions#new'
-	post '/login'    => 'sessions#create'
+  get '/login'  => 'sessions#new'
+	post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
   resources :resumes, only: [:index, :new, :create, :destroy]
   root "resumes#index"
