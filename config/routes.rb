977f6aa152df73cc280b316013cd1b924432b888
Luis  Senior <heybluguy@yahoo.com>
2018-12-05 22:48:42 -0600

    Adds resume mvc to upload and manage resumes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'resumes/index'
+  get 'resumes/new'
+  get 'resumes/create'
+  get 'resumes/destroy'
   root to: 'home#index'
   resources :projects
   get '/login'     => 'sessions#new'
 	post '/login'    => 'sessions#create'
-	get '/logout' => 'sessions#destroy'
+  get '/logout' => 'sessions#destroy'
+  resources :resumes, only: [:index, :new, :create, :destroy]
+  root "resumes#index"
 end
